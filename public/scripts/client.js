10f3df3c0ed75e6c0f57c3bec48824c614fb49ce
Logan Woolf <loganwoolf@gmail.com>
2022-02-01 21:38:41 +0000

    Fix incorrect ref to script

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,27 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(() => {
+
+  $('textarea').on({
+    input: function() {
+      const counter = $(this).next('div').children('output')[0];
+      counter.innerText = 140 - this.value.length;
+      if (this.value.length > 140) {
+        counter.style.color = '#FF0000';
+      } else {
+        counter.style.color = null;
+      }
+    }
+  });
+
+
+
+
+
+
+
+
+  
+
+});
